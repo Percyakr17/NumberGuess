a9b842a19fd47570a4e79817ec4c4349141a6416
Percyakr17 <samarth17garg@gmail.com>
2024-09-02 14:37:39 +0530

    adding css and JS supportive

--- script.js
+++ script.js
@@ -13,35 +13,29 @@ let enterNum
 let isStarted = false
 let checks
 
-function check(){
-    
+function check() {
     enterNum = numBox.value
-    if(isStarted == false){
+    if (!isStarted) {
         result.innerHTML = "Start the game first"
-    }else if(checks === 0){
-        result.innerHTML = "You lose! Start new game again"
-        updatechecks()
-        checkBtn.disabled = true
-    }
-    else if(enterNum != ""){
-        if(enterNum == orgNum){
+    } else if (checks === 0) {
+        showLosePrompt()
+    } else if (enterNum !== "") {
+        if (enterNum == orgNum) {
             result.innerHTML = "Congo ! You won :)"
             checkBtn.disabled = true
-        } else if(enterNum < orgNum){
+        } else if (enterNum < orgNum) {
             result.innerHTML = "Try a larger number"
             updatechecks()
-        } else{
+        } else {
             result.innerHTML = "Oh! Try a smaller number"
             updatechecks()
         }
-    }
-    else{
+    } else {
         result.innerHTML = "Where's the number?"
     }
-    
 }
 
-function newGame(){
+function newGame() {
     isStarted = true
     checkBtn.disabled = false
     orgNum = Math.floor(Math.random() * 20 + 1)
@@ -51,20 +45,31 @@ function newGame(){
     remChecks.innerText = "Remaining Checks : " + checks
 }
 
-function updatechecks(){
+function updatechecks() {
     checks--
     remChecks.innerText = "Remaining Checks : " + checks
-}
-
-function settingsOpen(){
-    if(settings.style.display === "none"){
-        settings.style.display = "block"
-    }else{
-        settings.style.display = "none"
+    if (checks === 0) {
+        showLosePrompt()
     }
 }
 
-function doneBtn(){
+function settingsOpen() {
+    settings.style.display = settings.style.display === "none" ? "block" : "none"
+}
+
+function doneBtn() {
     checks = changeChecks.value
     newGame()
+}
+
+function showLosePrompt() {
+    result.innerHTML = "You lose! Start a new game."
+    checkBtn.disabled = true
+    numBox.disabled = true
+
+    setTimeout(() => {
+        alert("Game Over! You've run out of checks. Better luck next time!")
+        numBox.disabled = false
+        remChecks.innerText = "Remaining Checks : 0"
+    }, 300)
 }
